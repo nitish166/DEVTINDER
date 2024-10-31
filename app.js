4edd74f0bac36415306458cd79d98d7c313f7f9e
Nitish Kumar <knitish123@gmail.com>
2024-10-31 14:30:50 +0530

    Playing with the routes

--- app.js
+++ app.js
@@ -5,24 +5,9 @@ const app = express();
 const PORT = 7777;
 
 
-// This will only handle GET call to /user
-app.get("/user", (req, res)=>{
-    res.send({Name: "Nitish", City: "Patna"});
-})
-
-app.post("/user", async (req, res)=>{
-    console.log(req.body);
-    res.send("Data successfully saved to the database");
-})
-
-app.delete("/user", (req, res)=>{
-    res.send("Deleted Successfully");
-})
-
-// This will match all the HTTP method API calls to /test
-
-app.use("/test", (req, res)=>{
-    res.send("Hello from the server");
+app.get("/user/:userId/:name/:password", (req, res)=>{
+    console.log(req.params);
+    res.send({firstName: "Nitish", lastName: "Patna"});
 })
 
 app.listen(PORT, ()=>{
--- homework.md
+++ homework.md
@@ -20,4 +20,7 @@
 - Install Postman app and make a workspace/collection > test API call
 - Write logic to handle GET, POST, PATCH, DELETE, API Calls and test them on Postman
 - Write logic to handle GET, POST, PATCH, DELETE API Calls and test them on Postman
-
+- Explore routing and use of ?, +, (), * in the routes
+- use of regex in routes /a, /.*fly$/
+- Reading the query parms in routes
+- Reading the dynamic routes
