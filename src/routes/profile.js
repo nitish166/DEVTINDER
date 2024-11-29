5ac89aef3fb90aaa8bb2f3e37dbc29795545c408
Nitish Kumar <knitish123@gmail.com>
2024-11-29 17:40:48 +0530

    forgot password API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const { validateEditProfileData } = require("../utils/validation");
+const bcrypt = require("bcrypt");
+const User = require("../modules/user");
 
 const profileRouter = express.Router();
 
@@ -31,6 +33,41 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     });
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  const userId = req.user._id;
+  const { oldPassword, newPassword } = req.body;
+
+  if (!oldPassword || !newPassword) {
+    return res
+      .status(400)
+      .send("Both oldPassword and newPassword are required !!!");
+  }
+
+  if (newPassword.length < 8) {
+    return res.status(400).send("New password must be 8 character long");
+  }
+
+  try {
+    const user = await User.findById(userId);
+    if(!user){
+      return res.status(404).send("User not found !!!");
+    }
+
+    const isMatch = await bcrypt.compare(oldPassword, user.password);
+    if(!isMatch){
+      return res.status(400).send("Old password is incorrect");
+    }
+
+    const hasPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hasPassword;
+    await user.save();
+
+    res.status(200).send("Password updated successfully !!!");
+  } catch (err) {
+    return res.status(500).send("ERROR : " + err.message);
   }
 });
 
